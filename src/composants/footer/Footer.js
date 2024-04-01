1866388d940f7f3b1b62ad68d36b61f569bca01b
Assoua Kouassi <assoua.enoc.salomon.kouassi@efrei.net>
2024-04-01 03:07:36 +0200

    01/04

--- src/composants/footer/Footer.js
+++ src/composants/footer/Footer.js
@@ -1,71 +1,18 @@
-import { Link } from 'react-router-dom';
+import {Link} from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { useEffect} from 'react';
+import ScrollToTop from "react-scroll-to-top";
 
 import './Footer.css';
-//import Legales from "../../pages/legales/legales.js";
 
 import twitter from '../../asset/icon/twitter-sign.png';
 import linkedin from '../../asset/icon/linkedin-logo.png';
 import github from '../../asset/icon/github-sign.png';
 
 export default function Footer() {
-    useEffect(() => {
-        function addLink() {
-            //Prevent from adding the link multiple times 
-            let footer = document.querySelector("footer")
-            console.log(footer);
-            let element = document.getElementById("toTopPage")
-
-            //Verify if element is inside the dom
-            if (!element)  {
-                console.log("here")
-                let linksContainer = document.createElement("div");
-                linksContainer.setAttribute("className", "row toTopPage-container");
-                let link = document.createElement("a");
-                link.href = "#header";
-                link.setAttribute("id", "toTopPage");
-                link.textContent = "Retourner en haut de la page";
-                linksContainer.append(link);
-                footer.append(link);
-            }
-        }
-
-        function removeLink() {
-            let link = document.getElementById("toTopPage");
-            if (link) {
-                let footer = document.querySelector("footer");
-                footer.removeChild(link);
-            }
-        }
-            
-        //Create a function to add a link on the bottom of the page
-        window.addEventListener("scroll", () => {
-            //Retrieve the value of the current height of the page 
-            //if we only take the document height the height will be too high
-            let documentHeight =  document.documentElement.scrollHeight;
-            //if we only take the window height the height will not change when we go on another page
-            let windowHeight = window.innerHeight;
-            //Page height is the actual visible/scrollable page 
-            let pageHeight = documentHeight - windowHeight;
-            //Get the value of the center of the page
-            let midPage= pageHeight * (4/5);
-            //Get the current value of the scrolling 
-            let actualValue = window.scrollY;
-            console.log(midPage)
-            console.log(actualValue)
-            //Check if actual value is superior than midPage to add the link
-            if (actualValue >= midPage) {
-                addLink();
-            }
-            if (actualValue < midPage) {
-                removeLink();
-            }
-        })
-    },[])
+    document.addEventListener("DOMContentLoaded", () => {})
     
-   return(
+    return(
         <footer className="footer container-fluid">
             <div className="row footer-info-container">
                 <div className="col-lg-3 col-md-6 footer__info">
@@ -78,7 +25,7 @@ export default function Footer() {
                             <p>Téléphone: <a href="tel:0751032770" className="links">0751032770</a></p>
                         </address>
                         <div className="container info__logo-container">
-                            <a href='github'><img src={github} alt="github-logo"/></a>
+                            <img src={github} alt="github-logo"/>
                             <img src={twitter} alt="twitter-logo"/>
                             <img src={linkedin} alt="linkedin-logo"/>
                         </div>
@@ -100,9 +47,9 @@ export default function Footer() {
                     <div className="container real__container">
                         <h3>Mes dernière realisations</h3>
                         <nav className="real__nav">
-                            <Link to="/Realisation" className="links"><FontAwesomeIcon icon={faChevronRight} className="links-chevron"/> Portfolio VueJS</Link>
-                            <Link to="/Realisation" className="links"><FontAwesomeIcon icon={faChevronRight} className="links-chevron"/> Formulaire Dynamique</Link>
-                            <Link to="/Realisation" className="links"><FontAwesomeIcon icon={faChevronRight} className="links-chevron"/> CV</Link>
+                            <Link to="/Realisations" className="links"><FontAwesomeIcon icon={faChevronRight} className="links-chevron"/> Portfolio VueJS</Link>
+                            <Link to="/Realisations" className="links"><FontAwesomeIcon icon={faChevronRight} className="links-chevron"/> Dynamic Form</Link>
+                            <Link to="/Realisations" className="links"><FontAwesomeIcon icon={faChevronRight} className="links-chevron"/> CV</Link>
                         </nav>
                     </div>
                 </div>
@@ -110,16 +57,17 @@ export default function Footer() {
                     <div className="container blog__container">
                         <h3>Mes derniers articles</h3>
                         <nav className="blog__nav">
-                            <Link to="BLog" className="links"><FontAwesomeIcon icon={faChevronRight} className="links-chevron"/> Coder son site en HTML/CSS</Link>
-                            <Link to="BLog" className="links"><FontAwesomeIcon icon={faChevronRight} className="links-chevron"/> Vendre ses produits sur le web</Link>
-                            <Link to="BLog" className="links"><FontAwesomeIcon icon={faChevronRight} className="links-chevron"/> Se positionner sur Google</Link>
+                            <Link to="/BLog" className="links"><FontAwesomeIcon icon={faChevronRight} className="links-chevron"/> Coder son site en HTML/CSS</Link>
+                            <Link to="/BLog" className="links"><FontAwesomeIcon icon={faChevronRight} className="links-chevron"/> Vendre ses produits sur le web</Link>
+                            <Link to="/BLog" className="links"><FontAwesomeIcon icon={faChevronRight} className="links-chevron"/> Se positionner sur Google</Link>
                         </nav>
                     </div>
                 </div>
             </div>
             <div className="row copyright-container">
                 <p className="copyright-container__text">&copy; Designed by John Doe</p>
-            </div>
+            </div>  
+            <ScrollToTop/> 
         </footer>
-   )
+    )
 }
